d52627d3482453b4fafba148e5d129be64b12e22
Marreco <fabio.marreco@accenture.com>
2018-09-28 13:58:53 -0300

    product type parser

--- playground/Parser.fsx
+++ playground/Parser.fsx
@@ -43,24 +43,32 @@ module Parsers =
         |> List.reduce Either
 
 
-    let Apply (p: Parser<'a>) (f: 'a -> 'b) : Parser<'b> =
+    let fmap (p: Parser<'a>) (f: 'a -> 'b) : Parser<'b> =
         let q stream =
             match p stream with
             | Success(x, rest) -> Success(f x, rest)
             | Failure -> Failure
         in q
 
-    let map f p = Apply p f
+
+    let map f p = fmap p f
+
+
+    let apply (f:Parser<'a -> 'b>) (p:Parser<'a>) : Parser<'b> = 
+        f >>= (fun f' -> fmap p f')
+
+    let (<*>) = apply
+
 
     let DigitParser = ['0'..'9'] |> List.map CharParser |> List.reduce Either
 
-    let DigitParserInt = Apply DigitParser (fun c -> (int c) - (int '0'))
+    let DigitParserInt = fmap DigitParser (fun c -> (int c) - (int '0'))
 
     let rec ZeroOrMore (p: Parser<'a>) : Parser<List<'a>> = 
         let q stream = 
             match p stream with 
             | Failure -> Success([], stream); 
-            | Success (x, rest) -> Apply (ZeroOrMore p) (fun t -> x::t) <| rest
+            | Success (x, rest) -> fmap (ZeroOrMore p) (fun t -> x::t) <| rest
         in q
 
 
@@ -68,10 +76,10 @@ module Parsers =
         let q stream = 
             match p stream with
             | Failure -> Failure
-            | Success (x, rest) -> Apply (ZeroOrMore p) (fun t -> x::t) <| rest
+            | Success (x, rest) -> fmap (ZeroOrMore p) (fun t -> x::t) <| rest
         in q
         
-    let UnsignedIntegerParser : Parser<int> = Apply (AtLeastOne DigitParserInt) (List.reduce (fun x y -> x * 10 + y))
+    let UnsignedIntegerParser : Parser<int> = fmap (AtLeastOne DigitParserInt) (List.reduce (fun x y -> x * 10 + y))
 
     let pSuccess x : Parser<'a> = 
         let p stream =  Success (x, stream)
@@ -83,7 +91,7 @@ module Parsers =
 
 
     let Combine (f:'a -> 'b -> 'c) (p1:Parser<'a>) (p2:Parser<'b>) : Parser<'c> = 
-        p1 >>= (fun a -> Apply p2 (fun b -> f a b))
+        p1 >>= (fun a -> fmap p2 (fun b -> f a b))
 
     let IntParser : Parser<int> = 
         let sign = CharParser '-' <|> CharParser '+' <|> pSuccess '+' |> map (function | '-' -> -1 | _ -> 1)
@@ -104,15 +112,15 @@ module Parsers =
 
     let FloatParser = 
         let prepend x l = x::l
-        let decParser  = CharParser '.' >>. Apply (ZeroOrMore DigitParserInt) (prepend 0 >> List.map float >> List.reduceBack (fun n acc -> acc / 10. + n))
-        Combine (+) (IntParser |> map float) decParser
+        let decParser  = CharParser '.' >>. fmap (ZeroOrMore DigitParserInt) (prepend 0 >> List.map float >> List.reduceBack (fun n acc -> acc / 10. + n))
+        Combine (+) (IntParser |> map float) (decParser <|> pSuccess 0.)
 
     let DecimalParser = 
         let prepend x l = x::l
-        let decParser  = CharParser '.' >>. Apply (ZeroOrMore DigitParserInt) (prepend 0 >> List.map decimal >> List.reduceBack (fun n acc -> acc / 10m + n))
+        let decParser  = CharParser '.' >>. fmap (ZeroOrMore DigitParserInt) (prepend 0 >> List.map decimal >> List.reduceBack (fun n acc -> acc / 10m + n))
         Combine (+) (IntParser |> map decimal) decParser
 
-    let (>>%) (p:Parser<'a>) (x:'b) = Apply p (fun _ -> x)
+    let (>>%) (p:Parser<'a>) (x:'b) = fmap p (fun _ -> x)
 
     let WhiteSpaceParser = CharParser ' ' |> AtLeastOne |> map List.length
 
@@ -131,9 +139,55 @@ module Parsers =
     let StringParser (s:string) = Seq.toList s |> CharListParser |> map (Array.ofList >> System.String >> string)
     let StringParserCI (s:string) = Seq.toList s |> CharListParserCI |> map (Array.ofList >> System.String >> string)
 
+    let EndOfFile : Parser<unit> = 
+        let p stream = 
+            match stream with
+            | [] -> Success ((), [])
+            | _ -> Failure
+        in p
+
+    let EndOfLine : Parser<char> = CharParser '\n' <|> (EndOfFile >>% '\000')
+
+    let EndOfWord = WhiteSpaceParser >>% ' ' <|> EndOfLine
+
+//    let EndOfWord = CharParser ' ' <|> CharParser '\n' <|> EOL
+
 
 
 open Parsers
+
+
+type Contract = 
+    | NTNB of int
+    | LTN of double 
+    | DI1
+
+
+let DUParser (prefix:string) (f:'a -> 'b) (p:Parser<'a>)  : Parser<'b> = 
+    StringParserCI prefix .>> WhiteSpaceParser >>. p |> map f
+
+let createTuple (item1:string) (item2:int) (item3:float)  = 
+    (item1, item2, item3)
+
+
+createTuple |> map <| (WordParser .>> WhiteSpaceParser) <*> (IntParser .>> WhiteSpaceParser) <*> FloatParser |> Parse "cassa 22 33"
+
+
+let fa (a:string) (b:int) = a , b
+
+let f1 = fa |> map <|  WordParser <*> WhiteSpaceParser  |> Parse "sadas  "
+
+let f2a f a b = f(a) + b
+let f2 = f2a fa <*> WhiteSpaceParser
+
+
+let ContractParser : Parser<Contract> = 
+    let ntnb = DUParser "ntnb" NTNB IntParser
+    let ltn = DUParser "ltn" LTN FloatParser
+    let di1 = StringParserCI "DI1" >>% DI1
+    ntnb <|> ltn <|> di1
+
+ContractParser |> Parse "ntnb"
 
 
 StringParserCI "Hello" |> Parse "helloWorld"
