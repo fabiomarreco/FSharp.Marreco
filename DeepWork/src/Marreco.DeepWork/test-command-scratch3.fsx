41efdcbb09df0bad23a26c6acffa27032fc581ca
Fabio Marreco <fabio.marreco@modal.com.br>
2019-07-18 10:22:38 -0300

    wip sample case

--- DeepWork/src/Marreco.DeepWork/test-command-scratch3.fsx
+++ DeepWork/src/Marreco.DeepWork/test-command-scratch3.fsx
@@ -1,22 +1,40 @@
+(* 
+ * Simple Example of CQRS in F# using Free Monads
+ * ==============================================
+ *   The goal is to create simple basic commands that define an algebra 
+ * for our data type; More complex commands will be built by composing these
+ * base commands much like lego.
+ *
+ * Domain:  
+ *    We will work with a very simple domain of a bank account, in which the
+ *  following actions can be peformed:
+ * 
+ *  - Accounts can be created, associated with a client 
+ *  - A client can have multiple accounts, but only one market as "Prefered"
+ *  - A deposit can be made to any account
+ *  - The account balance cannot become negative (overdraft)
+ *)
+
 type Client = string
 type Money = float
 type CmdError = string
+type Account = { Client : Client; Balance : Money }
 
 
 type DepositResult = 
     | Success | AccountDoesNotExists
 
-type CreateAccountResult = 
-    | Success | AccountAlreadyExisted
+type AccountCreationResult = 
+    | Success | AccountAlreadyExists
 
 type CommandF<'a> = 
-    | CreateAccount of Client * (CreateAccountResult -> 'a)
+    | CreateAccount of Client * (AccountCreationResult -> 'a)
     //| SetDepositLimit of Money * (unit -> 'a)
     | Deposit of Money * (DepositResult -> 'a)
 
 module CommandF = 
     let map f = function 
-        | Deposit (name, next) ->  Deposit (name, next >> f)
+        | Deposit (value, next) ->  Deposit (value, next >> f)
         | CreateAccount (qtd, next) -> CreateAccount (qtd, next >> f)
 
 type Command<'a> = 
@@ -62,16 +80,50 @@ type Event =
 
 //===================================================
 
-type Accounts = Map<Client, Money>
-
 module Account = 
-    let create client accounts = if (Map.containsKey client) then AccountAlreadyExisted | 
-    let deposit money account  = { account with Balance = account.Balance + money}
+    let create client = function 
+         | Some _ -> AccountAlreadyExists
+         | None -> AccountCreationResult.Success
+
+    let deposit money = function
+        | Some account -> DepositResult.Success
+        | None -> AccountDoesNotExists
 
 
-let interpretAsEventList events = function
-    | Pure a -> events
+module Event = 
+    let apply account = function
+        | AccountCreated client -> { Client = client; Balance = 0.}
+        | MoneyDeposited money -> { account with Balance = account.Balance + money }
+
+
+let rec interpretAsEventList (events, account) = function
+    | Pure a -> events |> List.rev
     | Free (CreateAccount (client, next)) -> 
-        
+        let res = Account.create client account
+        let cont = next res
+        match res with 
+        | AccountCreationResult.Success -> 
+            let event = AccountCreated client
+            let account' = Event.apply ({ Client = ""; Balance = 0.}) event |> Some // o option aqui nao esta legal
+            interpretAsEventList (event::events, account') cont
+        | AccountAlreadyExists -> 
+            interpretAsEventList (events, account) cont
+
+    | Free (Deposit (value, next)) -> 
+        let res = Account.deposit value account
+        let cont = next res
+        match res with 
+        | DepositResult.Success -> 
+            let event = MoneyDeposited value
+            let account' = Event.apply (account.Value) event |> Some
+            interpretAsEventList (event::events, account') cont
+        | AccountDoesNotExists -> 
+            interpretAsEventList (events, account) cont
+
+    
+createAccount "Marreco" |> interpretAsEventList ([], None) 
+deposit 10. |> interpretAsEventList ([], None) 
+
+depositCreatingAccount "Marreco" 10. |> interpretAsEventList ([], None) 
 
 
